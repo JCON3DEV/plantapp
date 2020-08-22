5dc813c5c364e621c377ed2041fd0e5610f0309e
JCON3DEV <jconheasman@gmail.com>
2020-08-22 23:23:35 +0000

    added a GET request to link index.ejs.

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
+app.get("/index", (req, res) => {
+  res.render("index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.render("index");
 });
