6c9a5be2c93fe3411daf251972a477547faaa64c
JCON3DEV <jconheasman@gmail.com>
2020-08-26 19:12:07 +0000

    fixed the typos and now the images show for the seeds

--- db/seeds/00_all_seeds.sql
+++ db/seeds/00_all_seeds.sql
@@ -39,19 +39,19 @@ VALUES ('Justin Reyes', 'austindotson@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD
 
 -- aloe vera plant - spikey
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (2, 18, true, 'Fasciata', 'aloe vera plant. Hardy, decorative and has medicianal uses.', '/public/docs/plants-1.jpg', '/public/docs/plants-1.jpg', 'plant', 'indoor plant', null, null);
+VALUES (2, 18, true, 'Fasciata', 'aloe vera plant. Hardy, decorative and has medicianal uses.', '/docs/plants/plant-1.jpg', '/docs/plants/plant-1.jpg', 'plant', 'indoor plant', null, null);
 -- multiple plants on a table
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
 VALUES (4, 35, true, 'Earth Pot', 'several pot plants on a table.', '/docs/pots/pot-1.jpg', '/docs/pots/pot-1.jpg', 'plant', 'indoor/outdoor plant', null, null);
 -- rubber tree broad leaf green plant
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 15, false, 'Rubber Tree', 'broad leafed green plant.', '/docs/plants/plants-2.jpg', '/docs/plants/plants-2.jpg', 'plant', 'plant', null, null);
+VALUES (1, 15, false, 'Rubber Tree', 'broad leafed green plant.', '/docs/plants/plant-2.jpg', '/docs/plants/plant-2.jpg', 'plant', 'plant', null, null);
 -- Pink Roses.
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 25, true, 'Pink Roses', 'Beautiful; flowering pink roases.', '/public/docs/plants-3.jpg', '/public/docs/plants-3.jpg', 'flower plant', null, null, null);
+VALUES (1, 25, true, 'Pink Roses', 'Beautiful; flowering pink roases.', '/docs/plants/plant-3.jpg', '/docs/plants/plant-3.jpg', 'flower plant', null, null, null);
 --  Snake Plant
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (3, 10, true, 'Snake Plant', 'Broad leaf with yellow edges. Stands vertically.', '/public/docs/plants-4.jpg', '/public/docs/plants-4.jpg', 'plant', null, null, null);
+VALUES (3, 10, true, 'Snake Plant', 'Broad leaf with yellow edges. Stands vertically.', '/docs/plants/plant-4.jpg', '/docs/plants/plant-4.jpg', 'plant', null, null, null);
 --  Macrame Pot
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
 VALUES (1, 30, true, 'A knotted Macrame Pot', 'Macramé, the art of knotting rope, is a trendy boho-chic inspired plant hanger design.', '/public/docs/pots/pot-2.jpg', '/public/docs/pots/pot-2.jpg', 'pots', null, 'clay', 'large');
