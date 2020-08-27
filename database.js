d231d3bd1a1b1b0924e1bf141dd1eb39cf411b23
whyttte <fogboruche@yahoo.com>
2020-08-27 23:44:59 +0000

    added getproductbysellerid

--- database.js
+++ database.js
@@ -583,3 +583,22 @@ const getOrderHistory = function() {
 };
 
 exports.getOrderHistory = getOrderHistory;
+
+
+const getProductsBySellerId = function(id) {
+  const querryString =
+  (`
+    SELECT *
+    FROM products
+      JOIN sellers on seller_id = sellers(id)
+    WHERE id = $1;
+  `)
+  return pool.query(querryString, [id])
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch (err => {
+      console.log('Error:', err)
+    });
+};
+exports.getProductsBySellerId = getProductsBySellerId;
