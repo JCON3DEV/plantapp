4eca1a65d7a9debdb7768e411d0d45eda9182e99
JCON3DEV <jconheasman@gmail.com>
2020-08-28 02:57:25 +0000

    corrected db issues with incorect pictures.

--- db/seeds/03_products_seeds.sql
+++ db/seeds/03_products_seeds.sql
@@ -1,6 +1,6 @@
 -- aloe vera plant - spikey
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (2, 18, true, 'Fasciata', 'aloe vera plant. Hardy, decorative and has medicianal uses.', 'public/docs/plants/plants-1.jpg', 'public/docs/plants/plants-1.jpg', 'plant', 'indoor plant', null, null);
+VALUES (2, 18, true, 'Fasciata', 'aloe vera plant. Hardy, decorative and has medicianal uses.', 'docs/plants/plant-1.jpg', 'docs/plants/plant-1.jpg', 'plant', 'indoor plant', null, null);
 
 -- multiple plants on a table
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
@@ -8,28 +8,28 @@ VALUES (4, 35, true, 'Earth Pot', 'several pot plants on a table.', '/docs/pots/
 
 -- rubber tree broad leaf green plant
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 15, false, 'Rubber Tree', 'broad leafed green plant.', '/docs/plants-2.jpg', '/public/docs/plants-2.jpg', 'plant', 'plant', null, null);
+VALUES (1, 15, false, 'Rubber Tree', 'broad leafed green plant.', '/docs/plants/plant-2.jpg', '/docs/plants/plant-2.jpg', 'plant', 'plant', null, null);
 
 -- Pink Roses.
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 25, true, 'Pink Roses', 'Beautiful; flowering pink roases.', '/public/docs/plants-3.jpg', '/public/docs/plants-3.jpg', 'flower plant', null, null, null);
+VALUES (1, 25, true, 'Pink Roses', 'Beautiful; flowering pink roases.', '/docs/plants/plant-3.jpg', '/docs/plants/plant-3.jpg', 'flower plant', null, null, null);
 
 --  Snake Plant
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (3, 10, true, 'Snake Plant', 'Broad leaf with yellow edges. Stands vertically.', '/public/docs/plants-4.jpg', '/public/docs/plants-4.jpg', 'plant', null, null, null);
+VALUES (3, 10, true, 'Snake Plant', 'Broad leaf with yellow edges. Stands vertically.', '/docs/plants/plant-4.jpg', '/docs/plants/plant-4.jpg', 'plant', null, null, null);
 
 --  Macrame Pot
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 30, true, 'A knotted Macrame Pot', 'Macramé, the art of knotting rope, is a trendy boho-chic inspired plant hanger design.', '/public/docs/pots/pot-2.jpg', '/public/docs/pots/pot-2.jpg', 'pots', null, 'clay', 'large');
+VALUES (1, 30, true, 'A knotted Macrame Pot', 'Macramé, the art of knotting rope, is a trendy boho-chic inspired plant hanger design.', '/docs/pots/pot-2.jpg', '/docs/pots/pot-2.jpg', 'pots', null, 'clay', 'large');
 
 -- fake data
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 30, true, 'somepot4', 'long description 7', 'https://www.pexels.com/photo/assorted-flower-arrangements-947914/', 'https://www.pexels.com/photo/two-round-ceramic-potted-green-plants-and-liquid-filled-clear-drinking-glass-1166642/', 'pots', null, 'ceramic', 'medium');
+VALUES (1, 30, true, 'Rubber Plant', 'long description 7', '/docs/plants/plant-2.jpg', '/docs/plants/plant-2.jpg', 'pots', null, 'ceramic', 'medium');
 
 -- fake data
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (2, 32, true, 'someseed1', 'long description 8', 'https://www.pexels.com/photo/sunflower-during-daytime-69832/', 'https://www.pexels.com/photo/batch-close-up-cooking-dry-273838/', 'seeds', 'flowers', null, null);
+VALUES (2, 32, true, 'Rubber Plant', 'long description 8', '/docs/plants/plant-2.jpg', '/docs/plants/plant-2.jpg', 'seeds', 'flowers', null, null);
 
 -- fake data
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (3, 17, true, 'someseed2', 'long description 9', 'https://www.pexels.com/photo/white-flowers-in-tilt-shift-lens-4268531/', 'https://www.pexels.com/photo/blurred-background-chestnuts-close-up-color-219737/', 'seeds', 'plants', null, null);
+VALUES (3, 17, true, 'Rubber Plant', 'long description 9', '/docs/plants/plant-2.jpg', '/docs/plants/plant-2.jpg', 'seeds', 'plants', null, null);
