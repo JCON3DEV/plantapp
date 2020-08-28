014f17510be163a33344131d0e5ef57ce8796b30
JCON3DEV <jconheasman@gmail.com>
2020-08-28 02:26:15 +0000

    updated the db with pictures

--- db/seeds/00_all_seeds.sql
+++ db/seeds/00_all_seeds.sql
@@ -57,13 +57,13 @@ INSERT INTO products (seller_id, price, availability, title, description, thumbn
 VALUES (1, 30, true, 'Macrame Pot', 'Macramé, the art of knotting rope, is a trendy boho-chic inspired plant hanger design.', '/docs/pots/pot-2.jpg', '/docs/pots/pot-2.jpg', 'pots', null, 'clay', 'large');
 -- fake data
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (1, 30, true, 'somepot4', 'long description 7', 'https://www.pexels.com/photo/assorted-flower-arrangements-947914/', 'https://www.pexels.com/photo/two-round-ceramic-potted-green-plants-and-liquid-filled-clear-drinking-glass-1166642/', 'pots', null, 'ceramic', 'medium');
+VALUES (1, 30, true, 'Bonsai Tree', 'Pretend you are a giant. A relaxing hobby as well as a beautifyl plant', '/docs/plants/plant-6.jpg', '/docs/plants/plant-6.jpg', 'pots', null, 'ceramic', 'medium');
 -- fake data
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (2, 32, true, 'someseed1', 'long description 8', 'https://www.pexels.com/photo/sunflower-during-daytime-69832/', 'https://www.pexels.com/photo/batch-close-up-cooking-dry-273838/', 'seeds', 'flowers', null, null);
+VALUES (2, 32, true, 'Spikey Succulent', 'Spikey Potplant', '/docs/plants/plant-7.jpg', '/docs/plants/plant-7.jpg', 'seeds', 'flowers', null, null);
 -- fake data
 INSERT INTO products (seller_id, price, availability, title, description, thumbnail_image_url, product_image_url, category, type, material, size)
-VALUES (3, 17, true, 'someseed2', 'long description 9', 'https://www.pexels.com/photo/white-flowers-in-tilt-shift-lens-4268531/', 'https://www.pexels.com/photo/blurred-background-chestnuts-close-up-color-219737/', 'seeds', 'plants', null, null);
+VALUES (3, 17, true, 'cat-plant', 'Eats and eats and eats and then leaves.', '/docs/plants/plant-8.jpg', '/docs/plants/plant-7.jpg', 'seeds', 'plants', null, null);
 
 
 
